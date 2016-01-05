41404488e8385222ccc1c03b1ec2c68c99e3aa10
Balthazar Gronon <bgronon@gmail.com>
2016-01-05 14:29:12 +0100

    fix(core): another typo

--- src/aes.js
+++ src/aes.js
@@ -10,7 +10,7 @@ export default class AES {
   encrypt (text) {
     const aes = new sjcl.cipher.aes(this._key)
     const bits = sjcl.codec.utf8String.toBits(text)
-    const iv = sjcl.random.randowWords(3, 0)
+    const iv = sjcl.random.randomWords(3, 0)
     const cipher = sjcl.mode.ccm.encrypt(aes, bits, iv)
     const cipherIv = sjcl.bitArray.concat(iv, cipher)
     return sjcl.codec.base64.fromBits(cipherIv)
